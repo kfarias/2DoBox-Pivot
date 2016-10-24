d3f52083fec0ed770f51bb16ae5d408c676e6827
Kenia Farias <11431kenia@gmail.com>
2016-10-24 17:19:14 -0600

    Create upvote and downvote buttons with funcationality

--- javascript.js
+++ javascript.js
@@ -9,7 +9,9 @@ IdeaBox.prototype.createIdeaBox = function(){
     `<section class="idea-card">
        <h2>`+this.title+`</h2>
        <p>`+this.idea+`</p>
-       <p class="quality">`+this.quality+`</p>  
+       <p class="quality">`+this.quality+`</p>
+       <button class="up-vote">Up Vote</button>
+       <button class="down-vote">Down Vote</button>
      </section>
     `
   )
@@ -22,3 +24,36 @@ $(".submit-btn").on("click", function(){
   var ideabox = new IdeaBox(title, idea);
   ideabox.createIdeaBox();
 })
+
+$(".idea-container").on("click", ".up-vote, .down-vote", function(){
+  var selector = $(this).text();
+  var quality = $(this).closest(".idea-card").find(".quality");
+  var newQuality = getNewQuality(selector, quality.text());
+  quality.text(newQuality);
+})
+
+function getNewQuality(selector, quality){
+  if(selector === "Up Vote"){
+    return upVote(quality);
+  } else {
+    return downVote(quality);
+  }
+}
+
+function upVote(quality){
+  switch (quality) {
+    case "swill":
+      return "plausible";
+    case "plausible":
+      return "genius"
+  }
+}
+
+function downVote(quality){
+  switch (quality) {
+    case "genius":
+      return "plausible";
+    case "plausible":
+      return "swill"
+  }
+}
