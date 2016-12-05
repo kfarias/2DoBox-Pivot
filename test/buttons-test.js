6cee34d9c4fb655ef4b43603874e9e2e636d57f3
Kenia Farias <11431kenia@gmail.com>
2016-12-05 09:00:47 -0700

    sort-buttons some bugs

---
+++ test/buttons-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const Buttons = require('../lib/buttons.js')
+
+describe('saveButton', function(){
+
+})
--- test/index.js
+++ test/index.js
@@ -1,7 +1,1 @@
-const assert = require('chai').assert
-
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true)
-    })
-  })
+require('./buttons-test.js');
